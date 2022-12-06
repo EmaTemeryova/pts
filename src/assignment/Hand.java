bb9f8cb1089c4e6593155ab833a714220772a98f
EmaTemeryova <--global>
2022-12-06 19:49:59 +0100

    init commit

---
+++ src/assignment/Hand.java
@@ -0,0 +1,29 @@
+package assignment;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class Hand {
+    int playerIdx;
+
+    public Optional<List<Card>> pickCards(List<HandPosition> positions) {
+
+    }
+
+    public Map<HandPosition, Card> removePickedCardsAndRedraw() {
+
+    }
+
+    public void returnPickedCards() {
+
+    }
+
+    public HandPosition hasCardOfType(CardType type) {
+
+    }
+
+    public List<Card> getCards() {
+        return null;
+    }
+}
